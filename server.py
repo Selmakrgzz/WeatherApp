c9168532bbdcedc94a9d321abf195d3e61521121
Selma Karagøz <selma.krgz@outlook.com>
2024-03-27 17:13:12 +0100

    Added comments to the code for more explanations

--- server.py
+++ server.py
@@ -2,27 +2,42 @@ from flask import Flask, render_template, request
 from weather import get_current_weather
 from waitress import serve
 
+#Using Flask as framework for this project
 app=Flask(__name__)
 
+#When someone is entering the root address "/" or "/index"
+#the flask application will return the index.html page
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+#When someone is entering the root address "/weather"
+#the flask application will do the following
 @app.route('/weather')
 def get_weather():
+    #Get's the city parameter from the url
     city=request.args.get('city')
 
     #Check for emty strings or string with only spaces
+    #It will set city to konya province by default
     if not bool(city.strip()):
         city="Konya Province"
     
+    #Retrives the weather information using the get_current_weather 
+    #method from the weather.py file.
+    #This variable will now store the weather information as a json
     weather_data=get_current_weather(city)
 
     #City is not found by API
     if not weather_data['cod'] == 200:
+        #Returning the city not found html page where the user
+        #get's the opurtunity to try again
         return render_template('city-not-found.html')
 
+    #Returning the weather page to the user
+    #title,status etc goes back to the weather html page
+    #to be displayed for the user
     return render_template(
         "weather.html",
         title=weather_data["name"],
--- weather.py
+++ weather.py
@@ -5,6 +5,9 @@ from pprint import pprint #Pretty print
 
 load_dotenv()
 
+#Uses API to get the weather information for the given city name
+#Weather_data stores the weather information as a json
+#This method returns the weather information as a json
 def get_current_weather(city="Oslo"):
     requests_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
     
@@ -12,6 +15,7 @@ def get_current_weather(city="Oslo"):
 
     return weather_data
 
+#This will help us to run the weather app in the terminal
 if __name__ == "__main__":
     print('\n*** Get Current Weather Conditions ***\n')
     
@@ -22,9 +26,8 @@ if __name__ == "__main__":
     if not bool(city.strip()):
         city="Konya Province"
         
-
-
+    #Calls the get_current_weather method and sends in the city name from user input
     weather_data=get_current_weather(city)
 
     print("\n")
-    pprint(weather_data)
+    pprint(weather_data) #Prints the output in a readable format
